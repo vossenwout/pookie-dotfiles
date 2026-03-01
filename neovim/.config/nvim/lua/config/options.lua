378d878a32d4de60d4a827da2eceec22651b10c3
spookie 9000 <spookie@spookies-Mac-mini.local>
2026-03-01 18:28:00 +0100

    Better ssh

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -1,8 +1,22 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- vim.opt.clipboard = "unnamedplus"
 vim.opt.exrc = true
 vim.opt.secure = true
 vim.opt.cursorline = false
 vim.opt.formatoptions:remove({ "c", "r", "o" })
+
+if vim.env.SSH_CONNECTION then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+  vim.opt.clipboard = "unnamedplus"
+end
