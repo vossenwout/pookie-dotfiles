6fd4d9f6016a3a77974aadf5840c8de6684442ed
WoutVossen <vossen.w@hotmail.com>
2026-03-31 21:52:05 +0200

    1: relative numbers and leader

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,9 @@
+-- Set <space> as leader (must happen before other plugins loaded)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Relative line numbers
+vim.o.relativenumber = true
+
+-- Don't show mode (INSERT-NORMAL-...) in status line
+vim.o.showmode = false
