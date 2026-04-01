1fe585eadba57dbf63f8e87b97c82b3ed9cbeeb5
WoutVossen <vossen.w@hotmail.com>
2026-04-01 22:05:56 +0200

    2: diagnostics and yank highlights

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -7,3 +7,29 @@ vim.o.relativenumber = true
 
 -- Don't show mode (INSERT-NORMAL-...) in status line
 vim.o.showmode = false
+
+-- Sync vim and system clipboards
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+
+-- Raise dialog if you close unsaved buffer (prevent mistakes)
+vim.o.confirm = true
+
+-- Vim diagnostics
+vim.diagnostic.config({
+  severity_sort = true, -- show most severe error first
+})
+
+-- Show diagnostics
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, {desc = 'Show diagnostics'})
+
+-- Easily move between windows
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Highlight yanks 
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight-yank", {clear = true}),
+  callback = function() vim.highlight.on_yank() end,
+})
