5f93e186d75ccef8ac716b1084869df527e8dbe8
WoutVossen <vossen.w@hotmail.com>
2026-04-04 22:20:07 +0200

    Add treesitter

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -40,17 +40,24 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- Plugins
 vim.pack.add({
   'https://github.com/ibhagwan/fzf-lua',
+  'https://github.com/nvim-treesitter/nvim-treesitter' -- also $ brew install tree-sitter-cli
 })
 
 -- FzfLua Setup
 require('fzf-lua').setup({
  keymap = {
    builtin = {
-      ["<C-d>"]  = "preview-page-down",
-      ["<C-u>"]  = "preview-page-up",
+      ["<C-d>"]  = 'preview-page-down',
+      ["<C-u>"]  = 'preview-page-up',
    },
  },
 })
 
 vim.keymap.set('n', '<leader><leader>', '<cmd>FzfLua files<cr>', { desc = 'Find files'})
 vim.keymap.set('n', '<leader>/', '<cmd>FzfLua live_grep<cr>', { desc = 'Find live grep'})
+
+-- Treesitter
+vim.cmd('syntax off') -- Make it obvious if treesitter is missing
+vim.api.nvim_create_autocmd('FileType', {
+ callback = function() pcall(vim.treesitter.start) end,
+})
